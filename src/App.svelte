e861a1c9794c2d2dade3549d862ce8936770d457
Till Prochaska <mail@tillprochaska.de>
2020-12-09 16:46:55 +0100

    Add Bootstrap and dummy content
    Signed-off-by: Till Prochaska <mail@tillprochaska.de>

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,36 @@
-<main>
-    Hello World!
+<script>
+    import { title } from './stores.js';
+</script>
+
+<style>
+    main {
+        max-width: 42rem;
+    }
+</style>
+
+<main class="container py-4">
+    <h1>Review a submission</h1>
+    <p class="lead">Thanks for taking the time to review this paper submission. This form will guide you through the review process.</p>
+
+    <form>
+        <label
+            class="form-label"
+            for="title"
+            aria-describedby="title-help"
+        >
+            Submission title
+        </label>
+        <input
+            id="title"
+            class="form-control"
+            type="text"
+            required
+            bind:value={$title}
+        />
+        {#if $title}
+            <span id="title-help" class="form-text">
+                You’re reviewing a submission titled <strong>“{$title}”</strong>.
+            </span>
+        {/if}
+    </form>
 </main>
