94103d9a9d5ccc6f613901768fe6a919815bb839
Till Prochaska <mail@tillprochaska.de>
2020-12-29 15:27:17 +0100

    Add mail and download feature
    Signed-off-by: Till Prochaska <mail@tillprochaska.de>

--- src/Overview.svelte
+++ src/Overview.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { get } from 'svelte/store';
+
     import { 
         description, 
         resubmission,
@@ -15,16 +17,44 @@
     import Button from './Button.svelte';
     import OverviewSection from './OverviewSection.svelte';
 
+    const fields = [
+        { label: 'Description', store: description, step: 0, },
+        { label: 'Resubmission', store: resubmission, step: 0 },
+        { label: 'Clarity Of Exposition', store: clarityOfExposition, step: 1 },
+        { label: 'Quality of References', store: qualityOfReferences, step: 1 },
+        { label: 'Reproducibility', store: reproducibility, step: 1 },
+        { label: 'Rating', store: rating, step: 2 },
+        { label: 'Explanation of Rating', store: explanationOfRating, step: 2 },
+        { label: 'Reviewer Expertise', store: expertise, step: 3 },
+        { label: 'Private Comments', store: privateComments, step: 4 },
+    ];
+
+    function generatePlaintext() {
+        return fields
+            .map(({ label, store }) => `${label.toUpperCase()}\n${get(store)}`)
+            .join('\n\n');
+    }
+
     function print() {
         window.print();
     }
 
     function email() {
+        const subject = 'Review';
+        const body = encodeURIComponent(generatePlaintext());
+        const url = `mailto:reviews@example.org?subject=${subject}&body=${body}`;
 
+        window.open(url);
     }
 
     function download() {
+        const data = encodeURIComponent(generatePlaintext());
+        const url = `data:text/plain,${data}`;
 
+        const tag = document.createElement('a');
+        tag.setAttribute('href', url);
+        tag.setAttribute('download', 'review.txt');
+        tag.click();
     }
 </script>
 
@@ -40,15 +70,9 @@
         <Button style="primary small" on:click={download}>Download</Button>
     </div>
 
-    <OverviewSection label="Description" step={0} value={description} />
-    <OverviewSection label="Resubmission" step={0} value={resubmission} />
-    <OverviewSection label="Clarity of Exposition" step={1} value={clarityOfExposition} />
-    <OverviewSection label="Quality of References" step={1} value={qualityOfReferences} />
-    <OverviewSection label="Reproducibility" step={1} value={reproducibility} />
-    <OverviewSection label="Rating" step={2} value={rating} />
-    <OverviewSection label="Explanation of Rating" step={2} value={explanationOfRating} />
-    <OverviewSection label="Reviewer Expertise" step={3} value={expertise} />
-    <OverviewSection label="Private Comments" step={4} value={privateComments} />
+    {#each fields as { label, store, step }}
+        <OverviewSection label={label} step={step} value={store} />
+    {/each}
 
     <div class="hide-print">
         <Button style="primary small" on:click={print}>Print</Button>
