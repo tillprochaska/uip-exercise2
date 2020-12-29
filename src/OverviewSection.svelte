6f06acce9040a6e3006d6edf9347acaadeb77b67
Till Prochaska <mail@tillprochaska.de>
2020-12-29 14:15:49 +0100

    Prevent page breaks inside of overview section
    Signed-off-by: Till Prochaska <mail@tillprochaska.de>

--- src/OverviewSection.svelte
+++ src/OverviewSection.svelte
@@ -17,7 +17,7 @@ import { empty } from 'svelte/internal';
         width: 100%;
     }
 
-    .label h2 {
+    h2 {
         margin-right: var(--spacing-unit);
     }
 
@@ -25,21 +25,29 @@ import { empty } from 'svelte/internal';
         color: var(--color-text-light);
         font-style: italic;
     }
+
+    @media print {
+        .section {
+            break-inside: avoid;
+        }
+    }
 </style>
 
-<Stack space="xsmall">
-    <div class="label">
-        <h2 class="gamma">{label}</h2>
-        <div class="hide-print">
-            <Button style="secondary small" on:click={() => $currentStep = step}>Edit</Button>
+<div class="section">
+    <Stack space="xsmall">
+        <div class="label">
+            <h2 class="gamma">{label}</h2>
+            <div class="hide-print">
+                <Button style="secondary small" on:click={() => $currentStep = step}>Edit</Button>
+            </div>
         </div>
-    </div>
 
-    <p>
-        {#if $value}
-            {$value}
-        {:else}
-            <span class="empty">This field is empty.</span>
-        {/if}
-    </p>
-</Stack>
+        <p>
+            {#if $value}
+                {$value}
+            {:else}
+                <span class="empty">This field is empty.</span>
+            {/if}
+        </p>
+    </Stack>
+</div>
