bd0bc0b310a66b157e97faaf6fc7cccefbc0238d
Till Prochaska <mail@tillprochaska.de>
2021-01-02 11:27:19 +0100

    Slightly increase border radius
    Signed-off-by: Till Prochaska <mail@tillprochaska.de>

--- src/components/Notes.svelte
+++ src/components/Notes.svelte
@@ -29,9 +29,11 @@
 </script>
 
 <style>
-    table {
-        --border-radius: 5px;
+    .notes {
+        --border-radius: 10px;
+    }
 
+    table {
         width: 100%;
         border-collapse: collapse;
     }
@@ -67,9 +69,12 @@
         border-bottom-right-radius: var(--border-radius);
     }
 
-    td:focus-within {
+    td:not(.actions):focus-within {
         background-color: #fff;
-        outline: var(--input-border-width-focus) solid var(--input-border-color-focus);
+        outline-width: var(--input-border-width-focus);
+        outline-style: solid;
+        outline-color: var(--input-border-color-focus);
+        outline-offset: calc(-1 * var(--input-border-width-focus));
     }
 
     .actions {
@@ -100,7 +105,7 @@
         padding: var(--spacing-unit-l);
         background: var(--gray-100);
         text-align: center;
-        border-radius: 5px;
+        border-radius: var(--border-radius);
     }
 
     .empty-state p {
@@ -110,54 +115,56 @@
     }
 </style>
 
-{#if value.length <= 0}
-    <div class="empty-state">
-        <Stack>
-            <p>Add notes to your review to give authors feedback on specific parts of their submission.</p>
-            <Button on:click={addNote}>Add note</Button>
-        </Stack>
-    </div>
-{:else}
-    <Stack space="small">
-        <table>
-            <thead>
-                <tr>
-                    <th>Note</th>
-                    <th>Reference</th>
-                    <th></th>
-                </tr>
-            </thead>
-
-            <tbody>
-                {#each value as {note, reference}, index}
+<div class="notes">
+    {#if value.length <= 0}
+        <div class="empty-state">
+            <Stack>
+                <p>Add notes to your review to give authors feedback on specific parts of their submission.</p>
+                <Button on:click={addNote}>Add note</Button>
+            </Stack>
+        </div>
+    {:else}
+        <Stack space="small">
+            <table>
+                <thead>
                     <tr>
-                        <td>
-                            <textarea 
-                                id={`note-${index}`}
-                                placeholder="e.g. missing reference, spelling mistake, …"
-                                bind:value={note}
-                                use:focus
-                                use:autoresize
-                            />
-                        </td>
-                        <td>
-                            <textarea 
-                                id={`reference-${index}`}
-                                placeholder="e.g. page 4, paragraph 3"
-                                bind:value={reference}
-                                use:autoresize
-                            />
-                        </td>
-                        <td class="actions">
-                            <Button style="plain small danger" on:click={() => removeNote(index)}>
-                                <Icon data={trash} scale={1.25} label="Remove note" />
-                            </Button>
-                        </td>
+                        <th>Note</th>
+                        <th>Reference</th>
+                        <th></th>
                     </tr>
-                {/each}
-            </tbody>
-        </table>
-            
-        <Button style="primary small" on:click={addNote}>Add note</Button>
-    </Stack>
-{/if}
+                </thead>
+
+                <tbody>
+                    {#each value as {note, reference}, index}
+                        <tr>
+                            <td>
+                                <textarea
+                                    id={`note-${index}`}
+                                    placeholder="e.g. missing reference, spelling mistake, …"
+                                    bind:value={note}
+                                    use:focus
+                                    use:autoresize
+                                />
+                            </td>
+                            <td>
+                                <textarea
+                                    id={`reference-${index}`}
+                                    placeholder="e.g. page 4, paragraph 3"
+                                    bind:value={reference}
+                                    use:autoresize
+                                />
+                            </td>
+                            <td class="actions">
+                                <Button style="plain small danger" on:click={() => removeNote(index)}>
+                                    <Icon data={trash} scale={1.25} label="Remove note" />
+                                </Button>
+                            </td>
+                        </tr>
+                    {/each}
+                </tbody>
+            </table>
+
+            <Button style="primary small" on:click={addNote}>Add note</Button>
+        </Stack>
+    {/if}
+</div>
